da45b44090350373fa899ff808895a7edb01421d
Gabriel Soares <gabrsoares@outlook.com>
2022-12-13 16:51:31 -0300

    add divisao e multiplicacao & att calculadora

--- src/App.js
+++ src/App.js
@@ -40,11 +40,36 @@ const App = () => {
       setCurrentNumber('0');
       setOperation('-')
     } else {
-      const sum = Number(firstNumber) - Number(currentNumber);
-      setCurrentNumber(String(sum));
+      const minus = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(minus));
       setOperation('');
     }
   }
+
+  const handleMultNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('*');
+    } else {
+      const mult = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(mult));
+      setOperation('');
+    }
+  }
+
+  const handleDivideNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('/');
+    } else {
+      const divide = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(divide)).toFixed(2);
+      setOperation('');
+    }
+  }
+
   const handleEquals = () => {
 
     if(firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
@@ -55,6 +80,12 @@ const App = () => {
           case '-':
              handleMinusNumbers();
           break;
+          case '*':
+            handleMultNumbers();
+            break;
+          case '/':
+            handleDivideNumbers();
+            break;
           default:
           break;
         }
@@ -67,27 +98,33 @@ const App = () => {
       <Content>
         <Input value={currentNumber} />
         <Row>
-        <Button label="x" onClick={() => handleAddNumber('')} />
-        <Button label="/" onClick={() => handleAddNumber('')} />
+        <Button label="x²" onClick={() => handleAddNumber('')} />
+        <Button label="/" onClick={handleDivideNumbers} />
         <Button label="c" onClick={handleOnClear} />
-        <Button label="." onClick={() => handleAddNumber('')} />
+        <Button label="Cl" onClick={() => handleAddNumber('')} />
         </Row>
         <Row>
         <Button label="7" onClick={() => handleAddNumber('7')} />
         <Button label="8" onClick={() => handleAddNumber('8')} />
         <Button label="9" onClick={() => handleAddNumber('9')} />
-        <Button label="-" onClick={handleMinusNumbers} />
+        <Button label="x" onClick={handleMultNumbers} />
         </Row>
         <Row>
         <Button label="4" onClick={() => handleAddNumber('4')} />
         <Button label="5" onClick={() => handleAddNumber('5')} />
         <Button label="6" onClick={() => handleAddNumber('6')} />
-        <Button label="+" onClick={handleSumNumbers} />
+        <Button label="-" onClick={handleMinusNumbers} />
         </Row>
         <Row>
         <Button label="1" onClick={() => handleAddNumber('1')} />
         <Button label="2" onClick={() => handleAddNumber('2')} />
         <Button label="3" onClick={() => handleAddNumber('3')} />
+        <Button label="+" onClick={handleSumNumbers}/>
+        </Row>
+        <Row>
+        <Button label="%" onClick={() => handleAddNumber('1')} />
+        <Button label="0" onClick={() => handleAddNumber('2')} />
+        <Button label="." onClick={() => handleAddNumber('.')} />
         <Button label="=" onClick={handleEquals}/>
         </Row>
       </Content>
